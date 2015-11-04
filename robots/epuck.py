a0af5a7578568e7218134a6f51b16c470b05eed7
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2015-11-04 21:47:29 +0100

    Threaded interface revised

--- robots/epuck.py
+++ robots/epuck.py
@@ -2,7 +2,7 @@ from vrep import vrep
 from math import sqrt
 from numpy import mean, array, argmax
 from time import sleep
-from threading import Event
+from threading import Event, Condition
 from threading import Thread as ParralelClass
 # from multiprocessing import Process as ParralelClass
 
@@ -31,11 +31,11 @@ class Epuck(object):
         self.base_lenght = 7 * 10 ** -2
         
         self._prox_aliases = {"all" : range(8),
-                             "all-but-rear" : range(6),
-                             "front" : [2, 3],
-                             "rear" : [6, 7],
-                             "front-left" : [0, 1, 2],
-                             "front-right": [3, 4, 5]}
+                              "all-but-rear" : range(6),
+                              "front" : [2, 3],
+                              "rear" : [6, 7],
+                              "front-left" : [0, 1, 2],
+                              "front-right": [3, 4, 5]}
         
         self.avoid_fwd_spd = 0.1 
         self.avoid_rot_spd = 1.
@@ -47,6 +47,9 @@ class Epuck(object):
         self._behaviors = {}
         self._runnings = {}
         self._to_detach = {}
+
+        self._sensations = {}
+        self._conditions = {}
 
         # self._running = threading.Event()
         # self._running.set()
@@ -113,25 +116,12 @@ class Epuck(object):
         image.resize(resolution[0], resolution[1], 3)
         return image   
 
-    def attach_behavior(self, callback, freq=None):
-        if freq is None:
-            period = 1. / self.freq
-        else:
-            period = 1. / freq
-        self._runnings[callback.__name__] = Event()
-        self._to_detach[callback.__name__] = Event()
-        self._to_detach[callback.__name__].clear()
-        def loop(robot):
-            while True: 
-                if self._runnings[callback.__name__].is_set():
-                    callback(robot)
-                robot.wait(period)     
-                if self._to_detach[callback.__name__].is_set():
-                    break
-            self._to_detach[callback.__name__].clear()
-        self._behaviors[callback.__name__] = ParralelClass(target=loop, args=(self,))
-        self._runnings[callback.__name__].clear()
-        self._behaviors[callback.__name__].start()
+    def attach_behavior(self, behavior_callback, sensation_callback, freq=None):
+        self._sensations[sensation_callback.__name__] = Sensation(self, sensation_callback, Condition(), freq)
+        self._sensations[sensation_callback.__name__].start()        
+        self._behaviors[behavior_callback.__name__] = Behavior(self, self._sensations[sensation_callback.__name__], behavior_callback)
+        self._behaviors[behavior_callback.__name__].start()
+        return
 
     def detach_behavior(self, callback_name):
         if not callback_name in self._behaviors:
@@ -144,12 +134,80 @@ class Epuck(object):
         if not callback_name in self._behaviors:
             print("Warning: " + callback_name + " is not attached")
         else:
-            self._runnings[callback_name].set()  
-            self._to_detach[callback_name].clear() 
+            self._behaviors[callback_name]._running.set()  
+            self._behaviors[callback_name]._to_detach.clear() 
+
+    def start_all_behaviors(self):
+        for name, behavior in self._behaviors.iteritems():
+            behavior._running.set()
+            print "Behavior " + name + " started"
             
 
     def stop_behavior(self, callback_name):
         if not callback_name in self._behaviors:
             print("Warning: " + callback_name + " is not attached")
         else:
-            self._runnings[callback_name].clear()              
+            self._behaviors[callback_name]._running.clear()              
+
+
+    def attach_sensation(self, callback, freq=None):
+        self._sensations[callback.__name__] = Sensation(self, callback, Condition(), freq)
+        self._sensations[callback.__name__].start()
+
+
+class Sensation(ParralelClass):
+
+    def __init__(self, robot, callback, condition, freq=None):
+        ParralelClass.__init__(self)
+        if freq is None:
+            self.period = 1. / robot.freq
+        else:
+            self.period = 1. / freq        
+        self.robot = robot
+        self.callback = callback
+        self.condition = condition
+      
+    
+    def run(self):
+        while True:
+            self.condition.acquire()
+            if self.callback(self.robot):
+                self.condition.notify_all()
+            self.condition.release()
+            self.robot.wait(self.period)
+
+
+class Behavior(ParralelClass):
+
+    def __init__(self, robot, sensation, callback):
+
+        ParralelClass.__init__(self)
+        self.robot = robot
+        self.sensation = sensation
+        self.condition = sensation.condition
+        self.callback = callback
+        self._running = Event()
+        self._running.clear()
+        self._to_detach = Event()
+        self._to_detach.clear()          
+    
+    def run(self):
+
+        while True:
+            self.condition.acquire()
+            # print "acquired"
+            if self._to_detach.is_set():
+                self.condition.release()
+                break
+                # print "released for detach"
+                break
+            if self._running.is_set():
+                if self.sensation is None:
+                    # print "behave from None"
+                    self.callback(self.robot)
+                else:
+                    # print "waiting for sensation"
+                    self.condition.wait()
+                    # print "Received sensation"
+                    self.callback(self.robot)
+            self.condition.release()                 
