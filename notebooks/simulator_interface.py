8c2b6329067837359e3e353bd9cc7260773563f6
u119375 <U119375@DS.UPF.EDU>
2016-10-05 17:45:26 +0200

    add pypot import in simulator_interface.py

--- notebooks/simulator_interface.py
+++ notebooks/simulator_interface.py
@@ -1,5 +1,8 @@
 import sys
+from os.path import abspath
 sys.path.append('../..')
+sys.path.append('../../pypot')
+print(sys.path)
 
 from pypot.vrep import close_all_connections
 from pyvrep_epuck.vrep.simulator import get_session
