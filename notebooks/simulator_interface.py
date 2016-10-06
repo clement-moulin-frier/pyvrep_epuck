fc9c53cb9d69fdcd8d9bef4edcba044ad7200451
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2016-10-06 16:59:20 +0200

    upd

--- notebooks/simulator_interface.py
+++ notebooks/simulator_interface.py
@@ -3,6 +3,10 @@ sys.path.append('../..')
 sys.path.append('../../pypot')
 sys.path.append('../../enum34')
 
-from pypot.vrep import close_all_connections
-from pyvrep_epuck.vrep.simulator import get_session
+from functools import partial
 
+from pypot.vrep import close_all_connections
+from pyvrep_epuck.vrep.simulator import get_session as gs
+
+
+get_session = partial(gs, n_epucks=1, use_proximeters=[2, 3])
