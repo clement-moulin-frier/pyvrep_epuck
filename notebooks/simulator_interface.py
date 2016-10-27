6c2c182b68c80311e7f238602523482cb74424d8
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2016-10-27 15:53:02 +0200

    upd simulator_interface

--- notebooks/simulator_interface.py
+++ notebooks/simulator_interface.py
@@ -7,6 +7,9 @@ from functools import partial
 
 from pypot.vrep import close_all_connections
 from pyvrep_epuck.vrep.simulator import get_session as gs
+from pyvrep_epuck.vrep.simulator import close_session
 
 
-get_session = partial(gs, n_epucks=1, use_proximeters=[2, 3])
+get_session = partial(gs, n_epucks=1, use_proximeters=[1, 4])
+
+open_session = get_session
