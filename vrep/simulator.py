300e5e2d3f02e2234f75fe4642b700797e3476db
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2016-11-15 22:36:14 +0100

    sphere apparition and eating as simulator routines

--- vrep/simulator.py
+++ vrep/simulator.py
@@ -13,6 +13,8 @@ import threading
 from numpy.linalg import norm
 from numpy import array
 from numpy.random import rand
+
+from ..routine import Routine, RoutineManager
 from .observer import Observable
 
 
@@ -51,6 +53,41 @@ def close_session(simulator, *epucks):
     close_all_connections()
 
 
+def sphere_apparition(simulator, min_pos, max_pos):
+    if not hasattr(simulator, "n_spheres"):
+        simulator.n_spheres = 0
+    if not hasattr(simulator, "eatable_objects"):
+        simulator.eatable_objects = []       
+    min_pos = array(min_pos)
+    max_pos = array(max_pos)
+    name = "Sphere_" + str(simulator.n_spheres + 1)
+    pos = rand(3) * (max_pos - min_pos) + min_pos
+    simulator.io.add_sphere(name, pos, [0.1, 0.1, 0.1], 0.5)
+    simulator.eatable_objects.append(name)
+    simulator.io._inject_lua_code("simSetObjectSpecialProperty({}, {})".format(simulator.io.get_object_handle(name), vrep.sim_objectspecialproperty_detectable_all))
+    simulator.n_spheres += 1
+    for robot in simulator.robots:
+        robot.register_object(name)
+
+def sphere_eating(simulator):
+    if not hasattr(simulator, "eatable_objects"):
+        simulator.eatable_objects = []  
+        for i_r, r in enumerate(simulator.robots):
+            simulator.subscribe(topic=(i_r, "eat"), subscriber=r)              
+    objects_to_remove = []
+    for i_r, robot in enumerate(simulator.robots):
+        robot_pos = array(robot.position())
+        for obj in simulator.eatable_objects:
+            try:
+                obj_pos = array(simulator.object_position(obj))
+            except VrepIOErrors:
+                break
+            if obj not in objects_to_remove and norm(robot_pos - obj_pos) < 0.1:
+                objects_to_remove.append(obj)
+                simulator.emit((i_r, "eat"), simulator.io.get_simulation_current_time())
+    for obj in objects_to_remove:
+        simulator.eatable_objects.remove(obj)
+        simulator.remove_object(obj)
 
 class Simulator(Observable):
     def __init__(self, vrep_host='127.0.0.1', vrep_port=19997, scene=None, start=False):
@@ -60,52 +97,40 @@ class Simulator(Observable):
         self.robots = []
 
         self.vrep_mode = vrep_mode
-
-        self.t = 0.
-        self.dt = 100.
-
-        self.sphere_apparition_period = 0
-
+        
         self.n_robots = 0
 
         self.suffix_to_epuck = {}
+        self.seconds_to_run = 0
 
-        self._thread = threading.Thread(target=lambda: self._run(0))
-        self._running = threading.Event()
-        self._running.clear()
+        self._condition = threading.Condition()
+        self.routine_manager = RoutineManager()
 
         Observable.__init__(self)
 
-    def run(self):
-        self._running.set()
-        self._thread.start()
-
-    def wait(self):
+    def wait(self, seconds):
         start = self.io.get_simulation_current_time()
         while self.io.get_simulation_current_time() - start < seconds:
             sleep(0.005)
 
-    def stop(self):
-        self._running.clear()
 
     def close(self):
-        self.stop()
-        while self._thread.isAlive():
-            sleep(0.1)
+        self.detach_all_routines()
         self.io.stop_simulation()
         self.io.close()
 
+    def object_position(self, object_name):
+        return array(self.io.get_object_position(object_name))
+
     def start_sphere_apparition(self, period=5., min_pos=[-1., -1., .1], max_pos=[1., 1., 1.]):
-        for i_r, r in enumerate(self.robots):
-            self.subscribe(topic=(i_r, "eat"), subscriber=r)
-        self.sphere_min_pos = array(min_pos)
-        self.sphere_max_pos = array(max_pos)
-        self.sphere_apparition_period = period
-        if not self._thread.isAlive():
-            self.run()
+        self.attach_routine(sphere_apparition, freq=1./period, min_pos=min_pos, max_pos=max_pos)
+        self.attach_routine(sphere_eating, freq=4.)
+        self.start_routine(sphere_apparition)
+        self.start_routine(sphere_eating)
 
     def stop_sphere_apparition(self):
-        self.sphere_apparition_period = 0
+        self.stop_routine(sphere_apparition)
+        self.stop_routine(sphere_eating)
 
     def _vrep_epuck_suffix(self, num):
         if num == 0:
@@ -120,7 +145,6 @@ class Simulator(Observable):
             suffix = "#" + name.split("#")[1]
         else:
             suffix = ""
-        # print self.suffix_to_epuck, suffix
         return self.suffix_to_epuck[suffix]
 
     def get_epuck(self, use_proximeters=range(8), verbose=False):
@@ -133,55 +157,37 @@ class Simulator(Observable):
         return self.robots[-1]
 
     def get_epuck_list(self, n_epucks, verbose=False):
-        return [self.get_epuck(verbose) for _ in range(n_epucks)]
+        return [self.get_epuck(verbose=verbose) for _ in range(n_epucks)]
 
     def remove_object(self, name):
         self.io.call_remote_api("simxRemoveObject", self.io.get_object_handle(name), sending=True)
 
 
-    def _run(self, seconds):
+    def attach_routine(self,callback, freq, **kwargs):
+        routine = Routine(self, callback, self._condition, freq, **kwargs)
+        self.routine_manager.attach(routine)
 
-        self._running.set()
+    def detach_routine(self, callback):
+        self.routine_manager.detach(callback)
+        print "Routine " + callback.__name__ + " detached"
 
-        n_objects = 0
-        self.object_names = []
-        last_sphere_t = self.io.get_simulation_current_time()
-        while not seconds or self.t < seconds * 1000.:
-            start_time = self.io.get_simulation_current_time()
 
-            objects_to_remove = []
-            for i_r, robot in enumerate(self.robots):
-                robot_pos = array(robot.position())
-                for obj in self.object_names:
-                    try:
-                        obj_pos = array(self.io.get_object_position(obj))
-                    except VrepIOErrors:
-                        break
-                    if obj not in objects_to_remove and norm(robot_pos - obj_pos) < 0.1:
-                        objects_to_remove.append(obj)
-                        self.emit((i_r, "eat"), self.io.get_simulation_current_time())
-            for obj in objects_to_remove:
-                self.object_names.remove(obj)
-                self.remove_object(obj)
+    def detach_all_routines(self):
+        self.routine_manager.detach_all()
 
-            if self.sphere_apparition_period and start_time > last_sphere_t + self.sphere_apparition_period:
-                name = "Sphere_" + str(n_objects + 1)
-                pos = rand(3) * (self.sphere_max_pos - self.sphere_min_pos) + self.sphere_min_pos
-                self.io.add_sphere(name, pos, [0.1, 0.1, 0.1], 0.5)
-                self.object_names.append(name)
-                self.io._inject_lua_code("simSetObjectSpecialProperty({}, {})".format(self.io.get_object_handle(name), vrep.sim_objectspecialproperty_detectable_all))
-                n_objects += 1
-                for robot in self.robots:
-                    robot.register_object(name)
-                last_sphere_t = start_time
+    def start_routine(self, callback):
+        if self.routine_manager.start(callback):
+            print "Routine " + callback.__name__ + " started"
 
-            while self.io.get_simulation_current_time() < start_time + self.dt / 1000.:
-                # print "wait"
-                sleep(self.dt / (100. * 1000))
+    def start_all_routines(self):
+        self.routine_manager.start_all()
 
-            self.t += self.dt / 1000.
+    def stop_routine(self, callback):
+        if self.routine_manager.stop(callback):
+            print "Routine " + callback.__name__ + " stopped"
 
-            if not self._running.is_set():
-                break
+    def stop_all_routines(self):
+        self.routine_manager.stop_all()
 
-        self._running.clear()
+    def check_routines(self):
+        self.routine_manager.check()
