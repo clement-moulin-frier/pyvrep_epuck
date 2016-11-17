716e695c43c05a5df9efa207a82dcdf09bdbf5bc
Clement Moulin-Frier <clement.moulinfrier@gmail.com>
2016-11-17 17:13:00 +0100

    add logger

---
+++ logger.py
@@ -0,0 +1,17 @@
+class Logger(object):
+    def __init__(self):
+        self.logs = {}
+    def add(self, topic, data):
+        if topic not in self.logs:
+            self.logs[topic] = [data]
+        else:
+            self.logs[topic].append(data)
+    def get_log(self, topic):
+        if topic not in self.logs:
+            print "No data in " + topic
+            return []
+        else:
+            return self.logs[topic]
+    def clear(self):
+        del self.logs
+        self.logs = {}
